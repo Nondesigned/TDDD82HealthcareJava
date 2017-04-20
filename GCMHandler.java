34d9a01ff901d393ef0dc96a89582a95be7f5fe5
seband <seban649@student.liu.se>
2017-04-20 09:41:12 +0200

    stringify isvideo

--- GCMHandler.java
+++ GCMHandler.java
@@ -118,7 +118,7 @@ public class GCMHandler{
         try {
             payload.put("TYPE","call");
             payload.put("CALLER",callerId);
-            payload.put("isVideo",isVideo);
+            payload.put("isVideo",isVideo.toString());
             JsonPost.put("data", payload);
             JsonPost.put("to", token);
             System.out.println(JsonPost.toString());
